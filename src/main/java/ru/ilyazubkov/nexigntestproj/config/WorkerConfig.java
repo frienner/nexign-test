6ec17cf50c68f1986fd3be8cc2d220f1a6ba1981
Ilya Zubkov <ilja-zubkov022@yandex.ru>
2024-11-27 20:14:43 +0700

    creds removed

--- src/main/java/ru/ilyazubkov/nexigntestproj/config/WorkerConfig.java
+++ src/main/java/ru/ilyazubkov/nexigntestproj/config/WorkerConfig.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
+@EnableTransactionManagement
 @EnableScheduling
 public class WorkerConfig {
 
